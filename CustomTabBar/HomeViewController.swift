a3208498f1ca68da3e44649c40601aad204e15d0
Barp24 <barpesahov@gmail.com>
2022-04-22 18:02:16 +0300

    +TableView +StudentsList

--- CustomTabBar/HomeViewController.swift
+++ CustomTabBar/HomeViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    let students = ["Dani", "Moshe"]
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        return students.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = students[indexPath.row]
+        return cell
+    }
 }
